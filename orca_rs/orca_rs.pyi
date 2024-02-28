5cec9ffbf846309bebcdd6f7a16c351920136271
Felix Scherz <felixwscherz@gmail.com>
2024-02-28 21:52:09 +0100

    chore: update type hints

--- orca_rs/orca_rs.pyi
+++ orca_rs/orca_rs.pyi
@@ -1,5 +1,23 @@
-from typing import Any
+from typing import TypedDict
 
+class SOCElement(TypedDict):
+    weight: float
+    real: float
+    imag: float
+    root: int
+    spin: int
+    ms: int
 
-def parse(filename: str) -> list[dict[str, Any]]:
-    """Parses orca output"""
+class SOCEigenvector(TypedDict):
+    n: int
+    energy: float
+    elements: list[SOCElement]
+
+class SOCMatrix(TypedDict):
+    eigenvectors: list[SOCEigenvector]
+
+class OrcaOutput(TypedDict):
+    soc_matrix: SOCMatrix
+
+def parse(filename: str) -> OrcaOutput:
+    """Parse orca output file"""
